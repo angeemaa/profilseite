ff191a462b6c068f8b7e4611620b80b302257356
Angela Amacna <6031@htl.rennweg.at>
2019-10-12 12:46:39 +0200

    profilseite

---
+++ angularjs-template-master/app/components/profilseite.js
@@ -0,0 +1,14 @@
+"use strict";
+
+app.component("profilseite", {
+    templateUrl: "components/profilseite.html",
+    controller: "ProfilseiteController",
+    bindings: {}
+});
+
+
+app.controller("ProfilseiteController", function ($log) {
+
+    $log.debug("ProfilseiteController()");
+
+});
